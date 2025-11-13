9dd5f878553d6b4d497dec9ca06b39491573a4de
LuizHenrique43245 <caladoluiz10120312@gmail.com>
2025-11-12 21:14:47 -0300

    Separação do HTML, CSS E JS

---
+++ script1.js
@@ -0,0 +1,26 @@
+
+    const texto = document.getElementById("cor");
+    const body = document.body;
+
+   
+    function mudar() {
+        if (texto.style.color === "white") {
+            texto.style.color = "black";
+        } else {
+            texto.style.color = "white";
+        }
+    }
+
+   
+    function trocar() {
+        if (body.style.backgroundColor === "grey") {
+            body.style.backgroundColor = "darkblue"; 
+        } else {
+            body.style.backgroundColor = "grey"; 
+        }
+    }
+
+    texto.addEventListener("click", function() {
+        mudar();   
+        trocar();  
+    });
