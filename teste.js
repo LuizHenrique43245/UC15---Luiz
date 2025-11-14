e2077a8f6926544d8abd3b5b1ffcd718283d5ff2
LuizHenrique43245 <caladoluiz10120312@gmail.com>
2025-11-13 21:10:29 -0300

    Atualização do Portfolio

---
+++ teste.js
@@ -0,0 +1,20 @@
+const img = document.getElementById('foto');
+
+img.tabIndex = 0;
+
+img.addEventListener('click', () => {
+
+    img.classList.toggle('is-expanded')
+}
+)
+
+img.addEventListener('keydown', (e) => {             
+    if (e.key === 'Enter' || e.key === ' ') {
+        img.classList.toggle('is-expanded');
+        e.preventdefault();
+    }
+})
+
+img.addEventListener('mouseenter', () => img.classList.add('is-expanded')) 
+
+img.addEventListener('mouseleave', () => img.classList.remove('is-expanded')) 
